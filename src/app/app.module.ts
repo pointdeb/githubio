4dde9817c6855f6fc0f580fbf0307c744940e2d4
Stephano Romain <stephano.romain@gmail.com>
2018-06-08 07:53:48 +0300

    finish integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { UserCardComponent } from './component/user-card/user-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    UserCardComponent
   ],
   imports: [
     BrowserModule
